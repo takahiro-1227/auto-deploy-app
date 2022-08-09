e92d7650a5a0ed98fd0390e17e2ced2e3618205c
佐藤孝洸 <satoutakashikou@mba.AirPort>
2022-08-09 09:35:55 +0900

    apiデプロイ

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,27 +1,16 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-lambda';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { typeDefs, resolvers, context } from './api';
-import express from 'express';
-import http from 'http';
 
-export const handler = async () => {
-  const app = express();
-  const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
+});
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context,
-    csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-  
-  server.applyMiddleware({ app, path: '/' });
-  
-  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000`);
-}
-handler();
+exports.handler = server.createHandler();
