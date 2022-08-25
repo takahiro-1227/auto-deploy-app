bd76f2fdbb2fd3c29d1ebb78db985d98f0768e05
佐藤孝洸 <satoutakashikou@mba.AirPort>
2022-08-25 21:22:05 +0900

    環境変数の読み込み法を変更

--- packages/front/webpack.config.js
+++ packages/front/webpack.config.js
@@ -1,27 +1,9 @@
-const dotenv = require('dotenv-safe');
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-const getEnvironmentVariables = env => {
-  const dotenvVariables = dotenv.config({ path: `${__dirname}/.env` }).parsed;
-
-  if (!dotenvVariables) {
-    throw new Error(`.envを設定してください。`);
-  }
-
-  // process.env.*のキーを設定して
-  const environmentVariables = Object.keys(dotenvVariables).reduce((keys, key) => {
-    const newKeys = keys;
-
-    newKeys[`process.env.${key}`] = JSON.stringify(dotenvVariables[key]);
-
-    return newKeys;
-  }, {});
-
-  return environmentVariables;
-};
-module.exports = env => ({
+module.exports = () => ({
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -34,7 +16,13 @@ module.exports = env => ({
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx']
   },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new webpack.DefinePlugin(getEnvironmentVariables(env))],
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new Dotenv({
+      path: './.env',
+      safe: true,
+    }),
+  ],
   devServer: {
     historyApiFallback: true,
     static: {
